e33de6bbdfefe099a980c9366dfade519186b7ab
Jamie Alavosus <alavosus.james@gmail.com>
2024-12-16 15:36:25 -0500

    Attempt to clean, maybe this will make the site work...

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import fs from 'fs';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	plugins: [sveltekit()]
 };
 
 export default config;
