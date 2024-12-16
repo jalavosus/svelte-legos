4429440c8820772b657fa07c469455df05705a06
Jamie Alavosus <alavosus.james@gmail.com>
2024-12-16 15:22:16 -0500

    Fix install command under /guides

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -4,13 +4,13 @@
 <div class="max-w-[700px]">
 	<h1 class="text-4xl font-light">Getting Started</h1>
 	<p class="mt-8 bg-slate-100 p-4 rounded-md border border-black">
-		Svelte<span class="text-prime">Legos (blue)</span> is a collection of utility functions based on Svelte
+		svelte-<span class="text-prime">legos (blue)</span> is a collection of utility functions based on Svelte
 		APIs. These utilities will help you bring the Browser APIs into your svelte components without writing
 		a lot of boilerplate code.
 	</p>
 
 	<div class="mt-16">
 		<h1 class="text-2xl">Installation</h1>
-		<p class="mt-8 bg-slate-100 p-4 rounded-md border border-black font-mono">npm i @jalavosus/svelte-legos</p>
+		<p class="mt-8 bg-slate-100 p-4 rounded-md border border-black font-mono">npm i @sveltelegos-blue/svelte-legos</p>
 	</div>
 </div>
