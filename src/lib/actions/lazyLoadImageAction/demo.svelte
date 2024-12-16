049788fcf6d3948c4dce1dee98b94edb6e99afbb
Jamie Alavosus <alavosus.james@gmail.com>
2024-12-16 16:45:20 -0500

    actions/lazyLoadImageAction: fix placeholder url

--- src/lib/actions/lazyLoadImageAction/demo.svelte
+++ src/lib/actions/lazyLoadImageAction/demo.svelte
@@ -7,5 +7,5 @@ import { PrimaryButtonClassName } from "$lib/shared/tailwind";
 <DemoContainer>
 	<div class="h-[500px]">Scroll, scroll, scroll</div>
 	<div class="mb-4">Below is an image which will load just now</div>
-	<img use:lazyLoadImageAction data-src="https://source.unsplash.com/8n7ipHhI8CI" alt="Testing" />
+	<img use:lazyLoadImageAction data-src="https://placecats.com/300/200" alt="Testing" />
 </DemoContainer>
